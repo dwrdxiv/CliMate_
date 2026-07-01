b57c1f8c8b345d5dfd2b0b761207c2d89050f322
dwrdxiv <adjfb.ed@gmail.com>
2026-07-01 19:13:25 -0400

    avance2

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,130 @@
 // src/app/page.js
+
+"use client";
+
+import WeatherStatCard from "@/components/weatherstatCard";
+import { Droplets, Wind, Flag, Search} from "lucide-react";
+import { useState, useEffect } from "react";
+
+
 export default function Home() {
-  return (
-    <main className="min-h-screen w-full bg-linear-to-b from-sky-300 to-blue-500 flex flex-col items-center p-6">
+  const [weather, setWeather] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API;
+
+  useEffect(() => {
+    async function initLocationFlow() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+            await fetchWeather(latitude, longitude);
+          },
+          async (error) => {
+            console.warn("Geolocation Level 1error:", error);
+            await runLevel2();   
+          });
+        } else {
+          await runLevel2();
+        }
+    }
+
+    async function runLevel2() {
+      try {
+        const res = await fetch("https://ipapi.co/json/");
+        const data = await res.json();
+        if (data.latitude && data.longitude) {
+          await fetchWeather(data.latitude, data.longitude);
+        } else {
+          runLevel3();
+        }
+      } catch (error) {
+        console.warn("Geolocation Level 1error:", error);
+        runLevel3();
+      }
+    }
+
+    function runLevel3() {
+      console.log("Level 3: Default Location");
+      fetchWeatherbyCity("Caracas");
+    }
+
+    // --- Funciones auxiliares de Fetch ---
+    async function fetchWeather(lat, lon) {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        );
+        const data = await res.json();
+        setWeather(data);
+      } catch (err) {
+        console.error("Error al obtener clima por coordenadas, aplicando fallback...");
+        runNivel3();
+      } finally {
+        setLoading(false);
+      }
+    }
+    async function fetchWeatherbyCity(city) {
+      try {
+        const res = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        const data = await res.json();
+        setWeather(data);
+      } catch (err) {
+        setErrorMsg("No se pudo obtener el clima de ninguna ubicación.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    initLocationFlow();
+  }, []);
+
       
+
+  return (
+    <main className="min-h-screen w-full bg-linear-to-b from-sky-300 from-40% to-blue-500 to-90% flex flex-col items-center p-6">
       {/* Sección del Buscador */}
       <header className="w-full max-w-md mt-5">
         <div className="relative group">
-          <input 
-            type="text" 
+          <input
+            type="text"
             placeholder="Looking for a city?"
-            className="w-full h-14 p-4 rounded-4xl bg-white/70 backdrop-blur-md border border-white text-black placeholder:text-black/50 outline-none drop-shadow-lg focus:ring-2 focus:ring-white/40 focus:scale-102 hover:scale-105 transition-all"
+            className="w-full h-12 p-4 rounded-4xl bg-linear-to-b from-white/70 from-70% to-gray-300/70 to-90% backdrop-blur-md border-2 border-white text-black placeholder:text-black/50 outline-none drop-shadow-md focus:ring-2 focus:ring-white/40 hover:scale-102 transition-all"
           />
-          <button className="absolute right-2 top-1/2 -translate-y-1/2 text-black bg-white/70 backdrop-blur-md rounded-4xl p-2 opacity-90 drop-shadow-lg hover:scale-105 transition-all">
-            Search
+          <button className="absolute items-center  right-1 top-1/2 w-10 h-10 -translate-y-1/2 text-black bg-white/90 backdrop-blur-md rounded-4xl p-2 opacity-90 drop-shadow-lg hover:scale-105 transition-all">
+            <Search size={22} color="blue" />
           </button>
         </div>
       </header>
 
-      {/* Ciudad Principal (Ubicación actual) */}
-      <section className="flex flex-col items-center justify-center m-10 h-72 w-72 p-4 rounded-4xl text-white bg-white/40 backdrop-blur-md border border-white drop-shadow-md hover:scale-102 transition-all">
-        <h1 className="text-8xl font-bold">24°</h1>
-        <p className="text-2xl font-medium">San Juan de los Morros</p>
-        <p className="text-lg opacity-80">Sunny</p>
+      {/* Frame principal */}
+      <section className="flex flex-col items-center m-10 h-58 w-58 p-4 rounded-4xl text-white bg-linear-to-b from-white/50 from-70% to-gray-300/50 backdrop-blur-md border-2 border-white drop-shadow-md hover:scale-102 transition-all">
+        <h1 className="text-8xl font-bold">
+          {Math.round(weather?.main?.temp)}°
+        </h1>
+        <p className="text-lg font-medium">{weather?.weather?.[0]?.main}</p>
+        <p className="text-sm opacity-70 text-gray-600">{weather?.name}</p>
+
+        <div className="flex items-center justify-center gap-3 mt-">
+          {/* Cajita 1: Humedad */}
+          <WeatherStatCard
+            icon={Droplets}
+            value={`${weather?.main?.humidity}%`}
+          />
+          {/* Cajita 2: País (Bandera) */}
+          <WeatherStatCard
+            icon={Flag}
+            value={weather?.sys?.country}
+          />
+          {/* Cajita 3: Viento */}
+          <WeatherStatCard
+            icon={Wind}
+            value={`${Math.round(weather?.wind?.speed * 3.6)}`} /* Conversión de m/s a Km/h */
+          />
+        </div>
       </section>
 
       {/* Grid de Ciudades Populares (Filter/Map) */}
@@ -31,9 +134,7 @@ export default function Home() {
           <p className="font-bold">Caracas</p>
           <p>28°</p>
         </div>
-        
       </section>
-
     </main>
   );
 }
